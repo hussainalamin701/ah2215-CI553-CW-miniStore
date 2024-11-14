f0b0d54255cc6e118cddba016fcf5f889558b3d9
Alamin Hussain <hussainalamin701@gmail.com>
2024-11-14 18:13:11 +0000

    ran setup script created new improved basket feature for CW

--- catalogue/BetterBasket.java
+++ catalogue/BetterBasket.java
@@ -12,7 +12,19 @@ import java.util.Collections;
 public class BetterBasket extends Basket implements Serializable
 {
   private static final long serialVersionUID = 1L;
-  
+
+  @Override
+  public boolean add( Product pr )
+  {
+    for (Product prInList : this){
+      if (prInList.getProductNum().equals(pr.getProductNum())){
+        int quantity = pr.getQuantity() + prInList.getQuantity();
+        prInList.setQuantity(quantity);
+        return true;
+      }
+    }
+    return super.add( pr );     // Call add in ArrayList
+  }
 
   // You need to add code here
   // merge the items for same product,
